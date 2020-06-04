689c16009551918024e971375aca7b0921ee3952
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2020-06-04 03:34:32 -0300

    :arrow_up: Update index.tsx file

--- Booster/web/src/App.tsx
+++ Booster/web/src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
+import Routes from './routes';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!
-        </p>
-      </header>
-    </div>
-  );
+    <Routes />
+    );
 }
 
 export default App;
