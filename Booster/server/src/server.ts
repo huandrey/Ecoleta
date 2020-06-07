0230f3ac0729b6d67727fc4bb3c313a5b5ac298c
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2020-06-07 10:17:46 -0300

    :arrow_up: Update server.ts

--- Booster/server/src/server.ts
+++ Booster/server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const App = express();
 
@@ -9,5 +10,7 @@ App.use(cors());
 App.use(express.json());
 App.use(routes);
 App.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+App.use(errors());
 
 App.listen(3333);
